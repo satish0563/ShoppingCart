b052011517a4f969202db54f05b01b259ac89362
satish <satishkr98@gmail.com>
2021-12-27 12:55:00 +0530

    Duplicate

--- src/Dashboard/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Content from "./Content";
-export default function ({ currentTheme, setCurrentTheme }) {
- return (
-    <Content>
-      
-    </Content>
-  );
-}
