76e17dd44a0154bc61d347a9bf88ff7d4a91a9f9
satish <satishkr98@gmail.com>
2021-12-27 22:34:21 +0530

    Add files via upload

---
+++ build/build/precache-manifest.ce8754f3cb3f292e1d6986fb6b0a6b83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ea59a3c9c6891f5dbcd97cceeb671f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd7b116d95109f5dc1b8",
+    "url": "/static/js/2.92ea8f6e.chunk.js"
+  },
+  {
+    "revision": "98d64a539c5fd0702fecc3a2def45cc5",
+    "url": "/static/js/2.92ea8f6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad490a581bc14c1df6eb",
+    "url": "/static/js/main.0ddf130b.chunk.js"
+  },
+  {
+    "revision": "26364f6e718db375e74d",
+    "url": "/static/js/runtime-main.24bb30b6.js"
+  }
+]);
